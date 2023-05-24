7706cebb0d403ea72d559e4d25015584dff78fff
Yondime <71274567+Yondime@users.noreply.github.com>
2023-05-24 17:48:31 +0300

    Create shikiplayer.js

---
+++ shikiplayer.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name            Shikiplayer for IOS
+// @description     Adds Kodik player to shikimori.me website
+// @icon            https://raw.github.com/qt-kaneko/Shikiplayer/main/images/icons/icon128.png
+// @version          1.0
+//
+// @match           *://shikimori.me/*
+//
+// @updateURL       https://raw.githubusercontent.com/Yondime/ShikiplayerIOS/main/shikiplayer.js
+// @downloadURL     https://raw.githubusercontent.com/Yondime/ShikiplayerIOS/main/shikiplayer.js
+//
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/CONFIG.js
+//
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/Boost.js/debugging.js
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/Boost.js/typization.js
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/Boost.js/querySelector.js
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/Boost.js/insertAfter.js
+//
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/Kodik.js
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/Shikimori.js
+// @require         https://raw.github.com/qt-kaneko/Shikiplayer/main/scripts/Shikiplayer.js
+// @run-at          document-end
+// ==/UserScript==
